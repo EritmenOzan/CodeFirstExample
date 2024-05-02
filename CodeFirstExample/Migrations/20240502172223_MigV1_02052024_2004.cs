ef32f01678499d815961cb1f44e2efe2bc72c887
Ulaş <Ula@DESKTOP-HURI6KA>
2024-05-02 20:35:27 +0300

    Proje dosyası ekle.

---
+++ CodeFirstExample/Migrations/20240502172223_MigV1_02052024_2004.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeFirstExample.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigV1_02052024_2004 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Owners",
+                columns: table => new
+                {
+                    OwnerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OwnerName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owners", x => x.OwnerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vessels",
+                columns: table => new
+                {
+                    VesselId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VesselName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VesselYear = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vessels", x => x.VesselId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Owners");
+
+            migrationBuilder.DropTable(
+                name: "Vessels");
+        }
+    }
+}
---
+++ CodeFirstExample/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+
+
+
+public class VesselTrackingContext : DbContext
+{
+    public DbSet<Vessel> Vessels { get; set; }
+    public DbSet<Owner> Owners { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost;Database=VesselTrackingDb;Integrated Security=true;Encrypt = False");
+    }
+}
+
+public class Vessel
+{
+    public int VesselId { get; set; }
+    public string VesselName { get; set; }
+    public int VesselYear { get; set;}  
+}
+
+public class Owner
+{
+    public int OwnerId { get; set; }
+    public string OwnerName { get; set; }
+}
