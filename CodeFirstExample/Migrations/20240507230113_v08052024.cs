6a8eca2411f88fa5f9ad2c8527a4076eae2a7923
Ulaş <Ula@DESKTOP-HURI6KA>
2024-05-08 02:07:46 +0300

    Classlar arası bağımlı ve bağımsız ilişki netleştirildi.

---
+++ CodeFirstExample/Migrations/20240507230113_v08052024.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeFirstExample.Migrations
+{
+    /// <inheritdoc />
+    public partial class v08052024 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "VesselId",
+                table: "Owners",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Owners_VesselId",
+                table: "Owners",
+                column: "VesselId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Owners_Vessels_VesselId",
+                table: "Owners",
+                column: "VesselId",
+                principalTable: "Vessels",
+                principalColumn: "VesselId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Owners_Vessels_VesselId",
+                table: "Owners");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Owners_VesselId",
+                table: "Owners");
+
+            migrationBuilder.DropColumn(
+                name: "VesselId",
+                table: "Owners");
+        }
+    }
+}
--- CodeFirstExample/Program.cs
+++ CodeFirstExample/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 public class VesselTrackingContext : DbContext
 {
@@ -10,14 +11,18 @@ public class VesselTrackingContext : DbContext
     {
         optionsBuilder.UseSqlServer("Server=localhost;Database=VesselTrackingDb;Integrated Security=true;Encrypt = False");
     }
+    static void Main(string[] args)
+    {
+        // Programın başlangıç noktası burasıdır
+    }
 }
 
 public class Vessel
 {
     public int VesselId { get; set; }
     public string VesselName { get; set; }
-    public int VesselYear { get; set;}  
-    public Owner Owner { get; set; }
+    public int VesselYear { get; set; }
+   
 }
 
 public class Owner
@@ -25,4 +30,5 @@ public class Owner
     public int OwnerId { get; set; }
     public string OwnerName { get; set; }
     public Vessel Vessel { get; set; }
+
 }
